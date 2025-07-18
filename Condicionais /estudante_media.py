2ab69463eabdcf3823e0495a83d99209d574aacf
Marcus Vinicius da Silva Alves <silvamarcus24@hotmail.com>
2025-07-18 15:01:06 -0300

    Create estudante_media.py

---
+++ Condicionais /estudante_media.py
@@ -0,0 +1,54 @@
+'''
+Uma professora precisa de um programa que ajude a calcular a média final dos alunos e informe se foram aprovados, ficaram de recuperação ou reprovados. 
+As regras são:
+
+Média >= 7: Aprovado
+5 <= Média < 7: Recuperação
+Média < 5: Reprovado
+
+Escreva um programa que receba três notas como entrada e calcule a média final. Com base na média, exiba a situação do aluno.
+'''
+
+# Menu
+print("------------------------------------------------------------------------")
+print("\033[38;5;82m"
+    """
+      
+██████╗░██████╗░░█████╗░████████╗██╗░█████╗░░█████╗░███╗░░██╗██████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║██╔══██╗██╔══██╗████╗░██║██╔══██╗██╔══██╗
+██████╔╝██████╔╝███████║░░░██║░░░██║██║░░╚═╝███████║██╔██╗██║██║░░██║██║░░██║
+██╔═══╝░██╔══██╗██╔══██║░░░██║░░░██║██║░░██╗██╔══██║██║╚████║██║░░██║██║░░██║
+██║░░░░░██║░░██║██║░░██║░░░██║░░░██║╚█████╔╝██║░░██║██║░╚███║██████╔╝╚█████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░░╚════╝░
+
+██████╗░██╗░░░██╗████████╗██╗░░██╗░█████╗░███╗░░██╗
+██╔══██╗╚██╗░██╔╝╚══██╔══╝██║░░██║██╔══██╗████╗░██║
+██████╔╝░╚████╔╝░░░░██║░░░███████║██║░░██║██╔██╗██║
+██╔═══╝░░░╚██╔╝░░░░░██║░░░██╔══██║██║░░██║██║╚████║
+██║░░░░░░░░██║░░░░░░██║░░░██║░░██║╚█████╔╝██║░╚███║
+╚═╝░░░░░░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝
+
+      """
+       "\033[0m")
+
+print("------------------------------------------------------------------------\n")
+
+# Solicita as notas do aluno
+primeira_nota = float(input("Digite a primeira nota: "))
+segunda_nota = float(input("Digite a segunda nota: "))
+terceira_nota = float(input("Digite a terceira nota: "))
+
+# Calcula a média final
+media_final = (primeira_nota + segunda_nota + terceira_nota) / 3
+
+# Situação do aluno
+if media_final >=7:
+    situacao = "Aprovado"
+elif 5 <= media_final < 7:
+    situacao = "Recuperação"
+else:
+    situacao = "Reprovado"
+
+# Exibe a situação do aluno
+print(f"A média final é: {media_final:.2f}")
+print(f"Situação: {situacao}")

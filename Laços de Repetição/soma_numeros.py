72d9d1609a7dff9215a4cf54d2d4884fb3320374
Marcus Vinicius da Silva Alves <silvamarcus24@hotmail.com>
2025-07-19 11:19:20 -0300

    Create soma_numeros.py

---
+++ Laços de Repetição/soma_numeros.py
@@ -0,0 +1,13 @@
+'''
+Você está recebendo uma lista de valores representando os produtos de sua loja virtual e gostaria de calcular a soma total desses produtos para
+entender o desempenho financeiro semanal.
+Crie um programa para implementar a soma.
+'''
+
+valores = [10, 20, 30, 40, 50]
+soma = 0
+
+for valor in valores:
+    soma += valor
+    
+print(f"A soma total dos produtos é: {soma}")
